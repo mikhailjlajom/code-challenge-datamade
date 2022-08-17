b1598231273abfe3e8cd08899ecb3877cb22d796
Mikhail <mikhailj.lajom@gmail.com>
2022-08-16 18:38:46 -0700

    submitting coding challenge

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -13,12 +13,41 @@ class Home(TemplateView):
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
+    # visited following websites to learn more about python
+    # https://www.django-rest-framework.org/api-guide/exceptions/
+    # https://www.django-rest-framework.org/api-guide/responses/
+    # https://www.w3schools.com/python/python_conditions.asp
+    # https://www.programiz.com/python-programming/function
+
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+
+        try:
+            input_string = request.data
+            address_components, address_type = self.parse(input_string)
+
+            if(address_components is None) or (address_type is None):
+                return None
+            else:
+                details = (input_string, address_components, address_type)
+
+        except ParseError:
+            print("Request contains malformed data")
+        return Response(details)
+
+        # never learned python yet so had to do research about all of it
+        # https://usaddress.readthedocs.io/en/latest/
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+
+        try:
+            address_components, address_type = usaddress.tag(address)
+        
+        except usaddress.RepeatedLabelError as e :
+            print(e.original_string, "An exception occurred")
+            return None
+
         return address_components, address_type
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
 # Define test fixtures here.
+import pytest
+
+@pytest.fixtures
+def client():
+    address = '123 main st chicago il'
+    return address
